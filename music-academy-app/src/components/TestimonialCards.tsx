18878820ea7044b53f72a2ce6da70aafcfa783ea
varunn_ranaa <varunrana1777@gmail.com>
2026-08-21 17:30:16 +0530

    Testimonial Section

---
+++ music-academy-app/src/components/TestimonialCards.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
+
+const musicSchoolTestimonials = [
+    {
+      quote:
+        'Joining the music school transformed my understanding of music and helped me to truly discover my own sound. The instructors are world-class!',
+      name: 'Alex Johnson',
+      title: 'Guitar Student',
+    },
+    {
+      quote:
+        "The community and support at this school are unmatched. I've grown not just as a pianist, but also as a performer, thanks to their comprehensive approach.",
+      name: 'Samantha Lee',
+      title: 'Piano Student',
+    },
+    {
+      quote:
+        "This school offered me the tools and confidence to take my singing to the next level. I'm endlessly grateful for the personalized coaching.",
+      name: 'Michael Chen',
+      title: 'Vocal Student',
+    },
+    {
+      quote:
+        'As a violinist, finding the right mentor can be challenging, but this school matched me with a teacher who truly understands my goals and challenges.',
+      name: 'Emily Taylor',
+      title: 'Violin Student',
+    },
+    {
+      quote:
+        'The production courses here opened my eyes to the intricacies of music production. Highly recommend for any aspiring producers!',
+      name: 'Chris Morales',
+      title: 'Music Production Student',
+    },
+  ];
+
+function MusicSchoolTestimonialCards() {
+  return (
+
+    <div className="h-[40rem] w-full rounded-md flex flex-col antialiased bg-white dark:bg-black bg-grid-pattern items-center justify-center relative overflow-hidden">
+      
+      <h2 className="text-3xl font-bold text-center mb-8 z-10">Hear our Harmony: Voices of success</h2>
+
+
+      {/* Radial Dark Mask: Blends the grid pattern perfectly into the background edges */}
+      <div className="absolute inset-0 pointer-events-none bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black" />
+
+      <div className="relative z-20 w-full flex flex-col items-center justify-center">
+        <InfiniteMovingCards
+          items={musicSchoolTestimonials}
+          direction="right"
+          speed="slow"
+        />
+      </div>
+    </div>
+  );
+}
+
+
+export default MusicSchoolTestimonialCards;
