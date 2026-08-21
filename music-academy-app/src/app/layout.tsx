ee58d466e6ca1b05e192e6a022c393b2a2f89367
varunn_ranaa <varunrana1777@gmail.com>
2026-08-21 17:35:44 +0530

    root-layout

--- music-academy-app/src/app/layout.tsx
+++ music-academy-app/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
           <NavBar/>
         </div>
         <main>{children}</main>
+        <Footer/>
         </body>
     </html>
   );
